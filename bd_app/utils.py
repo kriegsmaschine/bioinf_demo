f2f0ff74fa9c178c91e9ebaab1443093774101ef
kriegsmaschine <scukras7@gmail.com>
2016-08-08 23:26:34 -0400

    create QuerySet filter for date_of_death in ClinDataForm() utils.py

--- bd_app/utils.py
+++ bd_app/utils.py
@@ -8,7 +8,7 @@ Use this function to take PatientForm QuerySet and
 convert it to a list of integers for the patients'
 ForeignKeys id's
 '''
-def convertQuerySetToList(id_queryset):
+def convertMultListQuerySet(id_queryset):
 
 	id_list = []
 	for values in id_queryset:
@@ -23,7 +23,7 @@ Use this function to take PatientForm QuerySet and
 convert it to a list of integers for the patients'
 ForeignKeys id's
 '''
-def testConvert(id_queryset):
+def convertSingleListQuerySet(id_queryset):
 	id_list = []
 	for values in id_queryset:
 		id_list.append(values)
@@ -49,17 +49,24 @@ def processPatientForm(pt_form):
 	id_queryset.append(Patient.objects.filter(cohort__in=pt_form['cohort'].value(),
 				   gender__in=pt_form['gender'].value()).values_list('id',flat=True))
 
-	id_list = convertQuerySetToList(id_queryset)
+	id_list = convertMultListQuerySet(id_queryset)
 
 	return id_list
 
 
 
+
+'''
+Function to perform database query on pt_keys from PatientForm()
+AND the user input selections from the Clinical Parameters
+'''
 def processClinDataForm(clin_form, pt_key):
 	'''
 	Function to process clin_form and perform querysets on form
 	parameters and the returned pt_keys from the pt_form
 	'''
+
+
 
 	'''
 	use this line and following IF statement when need to test 
@@ -74,14 +81,36 @@ def processClinDataForm(clin_form, pt_key):
 	else:
 		msi_selection = clin_form['msi'].value()
 
+
+
+
+	'''
+	use this IF statement to check if a selection was made for 'days_to_death'
+	if no selection was made then select all values in the database
+	if a selection is made, pass on list of selection values to query
+	'''
+	if len(ClinData.objects.filter(days_to_death__in=clin_form['days_to_death'].value())) < 1:
+		dtd_selection = convertMultListQuerySet(ClinData.objects.values_list('days_to_death'))
+	else:
+		dtd_selection = clin_form['days_to_death'].value()
+
+
+
+
+	'''
+	This is the QuerySet to filter on all clinical parameters
+	'''
 	id_queryset = []
-	id_queryset = ClinData.objects.filter(id__in=pt_key, msi__in=msi_selection). \
+	id_queryset = ClinData.objects.filter(id__in=pt_key, msi__in=msi_selection,
+		              days_to_death__in=dtd_selection). \
 			      values_list('id',flat=True)
 
 	if id_queryset == None:
+		#if filter returns no items in the QuerySet then search by pt_key from 
+		#the PatientForm()
 		id_list = pt_key
 	else:
-		id_list = testConvert(id_queryset)
+		id_list = convertSingleListQuerySet(id_queryset)
 
 	#may remove 'msival' from return dictionary, used for testing purposes
 	dict_clinData = {'id_list':id_list, 'msival':msi_selection}
@@ -90,6 +119,11 @@ def processClinDataForm(clin_form, pt_key):
 
 
 
+'''
+Function to retreive expression data by pt_keys from PatientForm()
+AND ClinDataForm()  also from user selections in the
+Gene of Interest
+'''
 def processExpDataForm(exp_form, pt_key):
 
 	exp_data = pt_key
